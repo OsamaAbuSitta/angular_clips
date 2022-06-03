5c8afefa5b9450abef94426b43c7bfde16f28534
Osama AbuSitta <osama.abusitta@gmail.com>
2022-06-03 21:23:29 +0400

    add login form

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Constants } from '@shared/constants';
 
 @Component({
   selector: 'app-login',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  constatns = Constants;
+
+  credentials = {
+    email : '',
+    password:''
+  };
 
   constructor() { }
 
